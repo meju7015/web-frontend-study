7769703a24f7eeb24ce3c4820cfff96e1c5f450b
정주호 <meju7015@gmail.com>
2021-03-03 16:05:04 +0900

    Add 과제 코드 수정

--- 1st/3. 과제 class.js
+++ 1st/3. 과제 class.js
@@ -4,87 +4,108 @@
     슬롯 (or template)
     이용하여 Custom modal 작성
  */
-class LgModal extends HTMLElement {
+class SimpleModal extends HTMLElement {
 
     constructor() {
         super();
 
-        this.shadow = this.attachShadow({mode: "open"});
+        this.attachShadow({mode: "open"});
+        this._visible = false;
 
-        let template = document.createElement('div');
-        let style = document.createElement('style');
-
-        template.innerHTML = `
-                <div class="modal-wrap">
-                    <div class="modal-box">
-                        <span class="close">&times;</span>
-                        <div class="modal-title">
-                            <slot name="title"></slot>
-                        </div>
-                        <div class="modal-body">
-                            <slot name="content"></slot>
-                        </div>
+        this.shadowRoot.innerHTML = `
+            <div class="modal-wrap">
+                <div class="modal-box">
+                    <span class="close">&times;</span>
+                    <div class="modal-title">
+                        <slot name="title"></slot>
                     </div>
-                </div>`;
-
-        style.textContent = `
-            .modal-wrap {
-                display: none;
-                position: fixed;
-                background-color: rgb(0, 0, 0);
-                background-color: rgba(0, 0, 0, 0.4);
-                z-index: 1;
-                left: 0;
-                top: 0;
-                width: 100%;
-                height: 100%;
-                overflow: auto;
-            }
-
-            .modal-box {
-                background-color: #efefef;
-                margin: 15% auto;
-                padding: 20px;
-                border: 1px solid #888;
-                width: 50%;
-                border-radius: 15px;
-            }
-
-            .close {
-                color: #aaa;
-                float: right;
-                font-size: 28px;
-                font-weight: bold;
-            }
-
-            .close:hover,
-            .close:focus {
-                color: black;
-                text-decoration: none;
-                cursor: pointer;
-            }`;
-
-        this
-            .appendShadowChild(style)
-            .appendShadowChild(template);
+                    <div class="modal-body">
+                        <slot name="content"></slot>
+                    </div>
+                </div>
+            </div>
+            
+            <style>
+                .modal-wrap {
+                    display: none;
+                    position: fixed;
+                    background-color: rgb(0, 0, 0);
+                    background-color: rgba(0, 0, 0, 0.4);
+                    z-index: 1;
+                    left: 0;
+                    top: 0;
+                    width: 100%;
+                    height: 100%;
+                    overflow: auto;
+                }
+    
+                .modal-box {
+                    background-color: #efefef;
+                    margin: 15% auto;
+                    padding: 20px;
+                    border: 1px solid #888;
+                    width: 50%;
+                    border-radius: 15px;
+                    -webkit-animation-name: animatetop;
+                    -webkit-animation-duration: 1s;
+                    animation-name: animatetop;
+                    animation-duration: 0.4s;
+                }
+    
+                .close {
+                    color: #aaa;
+                    float: right;
+                    font-size: 28px;
+                    font-weight: bold;
+                }
+    
+                .close:hover,
+                .close:focus {
+                    color: black;
+                    text-decoration: none;
+                    cursor: pointer;
+                }
+                
+                @-webkit-keyframes animatetop {
+                    from {top: -300px; opacity: 0}
+                    to {top: 0; opacity: 1}
+                }
+                
+                @keyframes animatetop {
+                    from {top: -300px; opacity: 0}
+                    to {top: 0; opacity: 1}
+                }
+            </style>
+        `;
     }
 
     /**
      * 가상 트리가 document 에 연결된후 콜백
      */
     connectedCallback() {
+        this._modal = this.shadowRoot.querySelector('.modal-wrap');
+        this.shadowRoot.querySelector('.close').addEventListener('click', this._hide.bind(this));
+
         this.shadowRoot.querySelector('.modal-box').style.backgroundColor
             = this.getAttribute('bgcolor');
-
-        this.shadowRoot.querySelector('.close').addEventListener('click', e => {
-            this.style.display = "none";
-        })
     }
 
-    appendShadowChild(child) {
-        this.shadow.appendChild(child);
-        return this;
+    /**
+     * 가상 트리가 document 에서 연결 해제 된 후 콜백
+     */
+    disconnectedCallback() {
+        this.shadowRoot.querySelector('.close').removeEventListener('click', this._hide);
+    }
+
+    _hide() {
+        this._visible = true;
+        this._modal.style.display = 'none';
+    }
+
+    show() {
+        this._visible = false;
+        this._modal.style.display = 'block';
     }
 }
 
-customElements.define('lg-modal', LgModal);
+customElements.define('simple-modal', SimpleModal);
